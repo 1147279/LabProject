caf7e5c9d3bb0f580dfe33d8e84204bb1cb16e93
1147279 <1147279@students.wits.ac.za>
2018-08-01 10:13:50 +0200

    Merge Fix

--- Code/CloudTests/ReqScript.sh
+++ Code/CloudTests/ReqScript.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/darren/Desktop/Proooooj/LabProject/Code/CloudTests/Req.txt  darrenblanckensee@10.0.0.110:/Users/darrenblanckensee/Desktop/LabProject/LabProject/Code/CloudTests
+spawn scp -r /home/darren/Desktop/Proooooj/LabProject/Code/CloudTests/Req.txt  darrenblanckensee@10.0.0.108:/Users/darrenblanckensee/Desktop/LabProject/LabProject/Code/CloudTests
 expect "password:"
 send "0116166224\r"
 expect "password:"
 send "0116166224\r"
 expect "password:"
 send "0116166224\r"
-expect "darrenblanckensee@10.0.0.110's password:"
+expect "darrenblanckensee@10.0.0.108's password:"
 send "0116166224\r"
