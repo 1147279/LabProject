a90534be323816251147518e95238ee5354df0f6
1168101 <1168101@students.wits.ac.za>
2018-08-09 23:28:38 +0200

    links for SQLite

--- SQLBusiness/test/main.cpp
+++ SQLBusiness/test/main.cpp
@@ -31,14 +31,7 @@ int main(int argc, char** argv) {
    rc = sqlite3_open("test.db", &db);
 
    /* Create SQL statement */
-   sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-         "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-         "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
-         "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
-         "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+  sql = "SELECT * from COMPANY";
 
    /* Execute SQL statement */
    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
