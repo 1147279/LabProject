02b766f63a165049029c37d1f43e67fff7ab2b08
1168101 <1168101@students.wits.ac.za>
2018-08-14 06:37:38 +0200

    Writing from database to csv file and reading commands from txt files

--- SQLBusiness/test/main.cpp
+++ SQLBusiness/test/main.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 #include <sqlite3.h> 
+#include <fstream>
 
 using namespace std;
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
-	/*
-   int i;
-   for(i = 0; i<argc; i++) {
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");*/
-   
-   
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) 
+{
 
    for(int i = 0; i<argc; i++) {
       cout << azColName[i] << argv[i] << endl;
@@ -22,21 +16,33 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
 }
 
 int main(int argc, char** argv) {
-   sqlite3 *db;
-   char *zErrMsg = NULL;
-   int rc;
-   const char *sql;
+	
+	ifstream input;
+	input.open("input.txt");
+	
+	sqlite3 *db;
+	char *zErrMsg = NULL;
+	int rc;
+	const char *sql;
 
-   /* Open database */
-   rc = sqlite3_open("test.db", &db);
+	/* Open database */
+	rc = sqlite3_open("test.db", &db);
 
-   /* Create SQL statement */
-  sql = "SELECT * from COMPANY";
+	/* Create SQL statement */
+	//sql = "SELECT * from COMPANY";
+	string tempString;
 
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-   
- 
-   sqlite3_close(db);
-   return 0;
+	
+	getline(input, tempString);
+	
+	cout << tempString << endl;
+	sql = tempString.c_str();
+	/* Execute SQL statement */
+	system("sqlite3 -header -csv 'test.db' 'select * from COMPANY;' > out.csv");
+	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+
+	sqlite3_close(db);
+	input.close();
+return 0;
 }
