13f849ec80ffde88d7c2666b1737c05700c58d9a
1147279 <1147279@students.wits.ac.za>
2018-08-20 13:30:02 +0200

    Middleton

---
+++ CloudQuery/middleton.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <sqlite3.h>
+#include <fstream>
+#include <ctime>
+#include <string>
+#include <unistd.h>
+#include <cstdlib>
+
+
+
+
+using namespace std;
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
+
+   for(int i = 0; i<argc; i++) {
+      cout << azColName[i] << argv[i] << endl;
+   }
+   cout << endl;
+
+   return 0;
+}
+
+
+
+int main(int argc, char** argv)
+{
+
+  unsigned int milli ;//= 500000;
+  milli = 100000;
+  string query;
+	sqlite3 *db;
+	char *zErrMsg = NULL;
+	int rc;
+	const char *sql;
+
+  cout << "Waiting For Request..." << endl;
+
+  ifstream inFileA;
+
+  while(1)
+  {
+    inFileA.open("reqQuery.txt",ifstream::in);
+    if (inFileA)
+    {
+      break;
+    }
+  }
+
+  usleep(milli);
+
+  inFileA.open("reqQuery.txt",ifstream::in);
+
+  inFileA >> query;
+
+  cout << query << endl;
+
+  rc = sqlite3_open("test2.db", &db);
+
+
+  system("sqlite3 -header -csv 'test2.db' 'select * from WEATHER;' > outTempAudrey.csv");
+
+
+  system("sudo service ssh start");
+
+  system("./audreyReply.sh");
+
+
+
+
+	sqlite3_close(db);
+	input.close();
+
+
+
+
+
+
+
+return 0;
+}
