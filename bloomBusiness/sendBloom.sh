566202b1dfe7acbd6fabbc442b02bb4821050383
1147279 <1147279@students.wits.ac.za>
2018-08-24 20:20:12 +0200

    Fixed Sends

--- bloomBusiness/sendBloom.sh
+++ bloomBusiness/sendBloom.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/bloomBusiness/resultingBitStringCompressed.txt  pi@10.0.0.110:/home/darren/Desktop/Proooooj/LabProject/bloomBusiness/
+spawn scp -r /home/pi/Desktop/LabProject/LabProject/bloomBusiness/resultingBitStringCompressed.txt  pi@10.0.0.110:/home/pi/Desktop/LabProject/LabProject/bloomBusiness/
 expect "password:"
 send "0116166224\r"
 expect "password:"
