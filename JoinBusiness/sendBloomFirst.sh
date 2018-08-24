ef5b7d6f8f46a5bc68fc2ada749bb8740ea588e6
1147279 <1147279@students.wits.ac.za>
2018-08-24 22:16:04 +0200

    fixes

--- JoinBusiness/sendBloomFirst.sh
+++ JoinBusiness/sendBloomFirst.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/JoinBusiness/compressedTempBitString.txt  pi@10.0.0.111:/home/pi/Desktop/LabProject/LabProject/JoinBusiness/
+spawn scp -r /home/pi/Desktop/LabProject/LabProject/JoinBusiness/compressedTempBitString.txt  pi@10.0.0.110:/home/pi/Desktop/LabProject/LabProject/JoinBusiness/
 expect "password:"
 send "0116166224\r"
 expect "password:"
 send "0116166224\r"
 expect "password:"
 send "0116166224\r"
-expect "pi@10.0.0.111's password:"
+expect "pi@10.0.0.110's password:"
 send "0116166224\r"
