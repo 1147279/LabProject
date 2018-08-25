c6a803550ee32365e42d9ae73a589da0b920aea6
1168101 <1168101@students.wits.ac.za>
2018-08-25 21:37:31 +0200

    Updated Join Business

--- JoinBusiness/test/main.cpp
+++ JoinBusiness/test/main.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
-#include <sqlite3.h> 
+#include <sqlite3.h>
 #include <fstream>
 
 using namespace std;
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName) 
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
 {
 
    for(int i = 0; i<argc; i++) {
       cout << azColName[i] << argv[i] << endl;
    }
    cout << endl;
-   
+
    return 0;
 }
 
 int main(int argc, char** argv) {
-	
+
 	cout << "Enter name of txt you will read from!"<< endl;
-	
+
 	string inputString;
 	string tempString;
-	
+
 	//inputString = "input.txt";
-	
+
 	ifstream infile;
 	ifstream input;
 	input.open("ForJoin.csv");
-	
+
 	sqlite3 *db;
 	char *zErrMsg = NULL;
 	int rc;
@@ -38,23 +38,23 @@ int main(int argc, char** argv) {
 
 	/* Create SQL statement */
 	//sql = "SELECT * from COMPANY";
-	
+
 	/*
-	
+
 	 CREATE TABLE TEMP(ID INT PRIMARY KEY NOT NULL, YEAR PRIMARY KEY INT NOT NULL, MONTH INT  NOT NULL, DAY  INT  NOT NULL, HOUR INT NOT NULL, MINUTE INT NOT NULL, SECOND INT NOT NULL, LOCATION CHAR(50) NOT NULL, TEMPERATURE FLOAT );
-	
+
 CREATE TABLE HUMID(ID INT PRIMARY KEY NOT NULL, YEAR PRIMARY KEY INT NOT NULL, MONTH INT  NOT NULL, DAY  INT  NOT NULL, HOUR INT NOT NULL, MINUTE INT NOT NULL, SECOND INT NOT NULL, LOCATION CHAR(50) NOT NULL, HUMIDITY FLOAT );
-	
+
 	*/
-	/*
+
 	cout << "A Split" << endl;
 	while(getline(input, tempString))
 	{
 		cout << tempString << endl;
 		tempString = "INSERT INTO TEMP (ID, YEAR, MONTH, DAY, HOUR, MINUTE, SECOND, LOCATION, TEMPERATURE) VALUES (" + tempString + ");";
-		
+
 		sql = tempString.c_str();
-		
+
 		rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 	}
 	input.close();
@@ -64,22 +64,26 @@ CREATE TABLE HUMID(ID INT PRIMARY KEY NOT NULL, YEAR PRIMARY KEY INT NOT NULL, M
 	{
 		cout << tempString << endl;
 		tempString = "INSERT INTO HUMID (ID, YEAR, MONTH, DAY, HOUR, MINUTE, SECOND, LOCATION, HUMIDITY) VALUES (" + tempString + ");";
-		
+
 		sql = tempString.c_str();
-		
+
 		rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 	}
 	cout << "C Split" << endl;
-	*/
-	string condition = "'select TEMP.YEAR, TEMP.MONTH, TEMP.DAY, TEMP.HOUR, TEMP.MINUTE, TEMP.SECOND, TEMP.LOCATION, "\
-	 " HUMID.YEAR, HUMID.MONTH, HUMID.DAY, HUMID.HOUR, HUMID.MINUTE, HUMID.SECOND, HUMID.LOCATION " \
+
+	string condition = "'select TEMP.YEAR, TEMP.MONTH, TEMP.DAY, TEMP.HOUR, TEMP.MINUTE, TEMP.SECOND,"\
+  " TEMP.LOCATION, TEMP.TEMPERATURE,"\
+	 " HUMID.YEAR, HUMID.MONTH, HUMID.DAY, HUMID.HOUR, HUMID.MINUTE, HUMID.SECOND, HUMID.LOCATION, " \
+   "HUMID.HUMIDITY "\
 	"FROM TEMP " \
-	"INNER JOIN HUMID ON TEMP.YEAR =HUMID.YEAR and TEMP.MONTH = HUMID.MONTH and TEMP.DAY = HUMID.DAY and "\
+	"INNER JOIN HUMID ON TEMP.YEAR = HUMID.YEAR and TEMP.MONTH = HUMID.MONTH and TEMP.DAY = HUMID.DAY and "\
 	" TEMP.HOUR = TEMP.HOUR and TEMP.MINUTE = HUMID.MINUTE and TEMP.SECOND = HUMID.SECOND and TEMP.LOCATION = HUMID.LOCATION;'";
-	condition = "sqlite3 -header -csv 'test.db' " +  condition  +" > Output.csv";
+
+  condition = "sqlite3 -header -csv 'test.db' " +  condition  +" > Output.csv";
+  cout << condition << '\n';
 	system(condition.c_str());
 	sqlite3_close(db);
-	
+
 	infile.close();
 return 0;
 }
