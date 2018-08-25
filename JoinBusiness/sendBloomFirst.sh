9273146791ef195387d1a36ea96ce5358815f9cf
1147279 <1147279@students.wits.ac.za>
2018-08-25 03:41:14 +0200

    New Sends

--- JoinBusiness/sendBloomFirst.sh
+++ JoinBusiness/sendBloomFirst.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/JoinBusiness/compressedTempBitString.txt  pi@10.0.0.110:/home/pi/Desktop/LabProject/LabProject/JoinBusiness/
+spawn scp -r /home/pi/Desktop/2/LabProject/JoinBusiness/compressedTempBitString.txt  pi@10.0.0.110:/home/pi/Desktop/2/LabProject/JoinBusiness/
 expect "password:"
 send "0116166224\r"
 expect "password:"
--- JoinBusiness/sendCSVForJoin.sh
+++ JoinBusiness/sendCSVForJoin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/JoinBusiness/ForJoin.csv  darren@10.0.0.119:/home/darren/Desktop/Proooooj/LabProject/JoinBusiness/
+spawn scp -r /home/pi/Desktop/2/LabProject/JoinBusiness/ForJoin.csv  darren@10.0.0.119:/home/darren/Desktop/Proooooj/LabProject/JoinBusiness/
 expect "password:"
 send "0116166224\r"
 expect "password:"
