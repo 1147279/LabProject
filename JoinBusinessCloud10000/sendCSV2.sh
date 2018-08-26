291534446f8800d97fbccc2f5fdbac025ded54e2
1147279 <1147279@students.wits.ac.za>
2018-08-26 02:45:43 +0200

    Sends

--- JoinBusinessCloud10000/sendCSV2.sh
+++ JoinBusinessCloud10000/sendCSV2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/2/LabProject/JoinBusinessCloud/outHum.csv  darren@10.0.0.119:/home/darren/Desktop/Proooooj/LabProject/JoinBusinessCloud/
+spawn scp -r /home/pi/Desktop/2/LabProject/JoinBusinessCloud10000/outHum.csv  darren@10.0.0.119:/home/darren/Desktop/Proooooj/LabProject/JoinBusinessCloud10000/
 expect "password:"
 send "0116166224\r"
 expect "password:"
--- JoinBusinessCloud10000/start.sh
+++ JoinBusinessCloud10000/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/darren/Desktop/Proooooj/LabProject/JoinBusinessCloud/joinReq.txt pi@10.0.0.110:/home/pi/Desktop/2/LabProject/JoinBusinessCloud/
+spawn scp -r /home/darren/Desktop/Proooooj/LabProject/JoinBusinessCloud10000/joinReq.txt pi@10.0.0.110:/home/pi/Desktop/2/LabProject/JoinBusinessCloud10000/
 expect "password:"
 send "0116166224\r"
 expect "password:"
