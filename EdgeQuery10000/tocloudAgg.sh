c51bc57b57422b108ac82f09ce9b7335825a39d3
1147279 <1147279@students.wits.ac.za>
2018-08-29 11:59:04 +0200

    IP Home

--- EdgeQuery10000/tocloudAgg.sh
+++ EdgeQuery10000/tocloudAgg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/darren/Desktop/Proooooj/LabProject/EdgeQuery10000/ForCloud.txt  uyanda@10.0.0.113:/home/uyanda/Desktop/ELEN4012/LabProject/EdgeQuery10000
+spawn scp -r /home/darren/Desktop/Proooooj/LabProject/EdgeQuery10000/ForCloud.txt  uyanda@10.0.0.113:/Users/darrenblanckensee/Desktop/ELEN4012/LabProject/EdgeQuery10000
 expect "uyanda@10.0.0.113's password:"
 send "heavens1\r"
 expect "uyanda@10.0.0.113's password:"
--- JoinBusiness10000/tocloud.sh
+++ JoinBusiness10000/tocloud.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/darren/Desktop/Proooooj/LabProject/JoinBusiness10000/Output.csv  uyanda@10.0.0.113:/home/uyanda/Desktop/ELEN4012/LabProject/relays
+spawn scp -r /home/darren/Desktop/Proooooj/LabProject/JoinBusiness10000/Output.csv  uyanda@10.0.0.113:/Users/darrenblanckensee/Desktop/ELEN4012/LabProject/relays
 expect "uyanda@10.0.0.113's password:"
 send "heavens1\r"
 expect "uyanda@10.0.0.113's password:"
