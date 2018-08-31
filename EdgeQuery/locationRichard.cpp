bf90cc9fd8df27fcbd94a2e945a44267d3349771
1147279 <1147279@students.wits.ac.za>
2018-08-31 11:56:38 +0200

    l

--- EdgeQuery/locationRichard.cpp
+++ EdgeQuery/locationRichard.cpp
@@ -23,6 +23,61 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName)
 }
 
 
+void initialiseString(string& str, int strSize)
+{
+	str = "";
+	for(int i = 0; i < strSize; i++)
+		str += "0";
+}
+
+
+
+
+bool bloomFilterContains (string& str, string& bitString)
+{
+	int hash1;
+	int hash2;
+	int hash3;
+	int asciNum = asciiSum(str);
+  cout << asciNum;
+  int num1;// = asciNum;
+  num1 = (asciNum-1555)*5112+2*asciNum; //+powerFunction((asciNum/2+powerFunction(num1, 2)),2)+3*(asciNum/23);
+
+	int num2 = asciNum ;
+  num2= powerFunction(num2, 3)+powerFunction((asciNum/3+powerFunction(asciNum, 3)),2)-3*(asciNum/21);
+
+	int num3 = asciNum ;
+  num3= (asciNum/1023)*(499*asciNum)-17825;//powerFunction(num3, 1)+powerFunction((num3/4+powerFunction(asciNum, 4)),2)+3*(asciNum/17);
+
+
+	hash1 = moding(num1, bitString);
+	hash2 = moding(num1, bitString);
+	hash3 = moding(num1, bitString);
+
+	if(bitString.at(hash1) == '0')
+		return false;
+
+	if(bitString.at(hash2) == '0')
+		return false;
+
+	if(bitString.at(hash3) == '0')
+		return false;
+
+
+	return true;
+}
+
+
+void setBloomFilter(string fileName, string& str)
+{
+	ifstream input;
+	input.open(fileName,ifstream::in);
+	str = "";
+	input >> str;
+
+	input.close();
+}
+
 
 int main(int argc, char** argv)
 {
