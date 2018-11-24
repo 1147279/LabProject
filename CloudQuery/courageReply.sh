ac24e6f89071972e89eb26fa9edfe155a8d52338
1147279 <1147279@students.wits.ac.za>
2018-11-24 14:38:40 +0200

    Updated Cloud Query

--- CloudQuery/courageReply.sh
+++ CloudQuery/courageReply.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/outTempCourage.csv  darren@10.0.0.114:/home/darren/Desktop/Proooooj/LabProject/CloudQuery/
+spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/outTempCourage.csv  pi@192.168.1.112:/home/pi/Desktop/LabProject/LabProject/CloudQuery/
 expect "password:"
 send "0116166224\r"
 expect "password:"
 send "0116166224\r"
 expect "password:"
 send "0116166224\r"
-expect "darren@10.0.0.114's password:"
+expect "pi@192.168.1.112's password:"
 send "0116166224\r"
--- CloudQuery/stormReply.sh
+++ CloudQuery/stormReply.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/outTempStorm.csv  darren@10.0.0.114:/home/darren/Desktop/Proooooj/LabProject/CloudQuery/
+spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/outTempStorm.csv  pi@192.168.1.113:/home/pi/Desktop/LabProject/LabProject/CloudQuery/
 expect "password:"
 send "0116166224\r"
 expect "password:"
 send "0116166224\r"
 expect "password:"
 send "0116166224\r"
-expect "darren@10.0.0.114's password:"
+expect "pi@192.168.1.113's password:"
 send "0116166224\r"
