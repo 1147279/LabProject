50a32d7ad585337d50de917569cb0bdf22a25862
1147279 <1147279@students.wits.ac.za>
2018-11-25 10:39:24 +0200

    fixed toclouds

--- CloudQuery/tocloudRichard.sh
+++ CloudQuery/tocloudRichard.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/FinalResulytRichard.csv  uyanda@192.168.1.100:/home/uyanda/Desktop/LabProject/LabProject/CloudQuery/
-expect "password:"
-send "0116166224\r"
-expect "password:"
-send "0116166224\r"
-expect "password:"
-send "0116166224\r"
+spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/FinalResultRichard.csv  uyanda@192.168.1.100:/home/uyanda/Desktop/LabProject/LabProject/CloudQuery/
+expect "uyanda@192.168.1.100's password:"
+send "heavens1\r"
+expect "uyanda@192.168.1.100's password:"
+send "heavens1\r"
 expect "uyanda@192.168.1.100's password:"
 send "heavens1\r"
