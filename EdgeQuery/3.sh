9a71007411548d793f6f2451fdcee7beb92218d9
1147279 <1147279@students.wits.ac.za>
2018-11-26 14:29:09 +0200

    replies fixed

--- EdgeQuery/3.sh
+++ EdgeQuery/3.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/outTempJohn.csv darren@10.0.0.114:/home/darren/Desktop/Proooooj/LabProject/CloudQuery/
+#Edge/bin/expect -f
+spawn scp -r /home/pi/Desktop/LabProject/LabProject/EdgeQuery/outTempRichard.csv  darren@10.0.0.114:/home/darren/Desktop/Proooooj/LabProject/EdgeQuery/
 expect "password:"
 send "0116166224\r"
 expect "password:"
--- EdgeQuery/audreyReply.sh
+++ EdgeQuery/audreyReply.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/EdgeQuery/outTempAudrey.csv  darren@10.0.0.114:/home/darren/Desktop/Proooooj/LabProject/EdgeQuery/
+spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/outTempAudrey.csv  darren@10.0.0.114:/home/darren/Desktop/Proooooj/LabProject/CloudQuery/
 expect "password:"
 send "0116166224\r"
 expect "password:"
