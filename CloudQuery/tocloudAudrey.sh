4e50e0e968cb057a58f9ba88e58704ea083bf178
1147279 <1147279@students.wits.ac.za>
2018-12-06 13:43:33 +0200

    Fixed Sends

--- CloudQuery/tocloudAudrey.sh
+++ CloudQuery/tocloudAudrey.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/FinalResultAudrey.csv  darren@192.168.1.110:/home/darren/Desktop/LabProject/LabProject/CloudQuery/
+spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/FinalResultAudrey.csv  darren@192.168.1.110:/home/darren/Desktop/Proooooj/LabProject/CloudQuery/
 expect "darren@192.168.1.110's password:"
 send "0116166224\r"
 expect "darren@192.168.1.110's password:"
