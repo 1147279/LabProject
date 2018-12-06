1da9fc4b502810ff15c978b8b3a349c3b7ca4b83
1147279 <1147279@students.wits.ac.za>
2018-12-06 13:02:15 +0200

    Fixed CLoud Query

--- CloudQuery/tocloudRichard.sh
+++ CloudQuery/tocloudRichard.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/expect -f
-spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/FinalResultRichard.csv  darren@192.168.1.114:/home/darren/Desktop/LabProject/LabProject/CloudQuery/
-expect "darren@192.168.1.114's password:"
+spawn scp -r /home/pi/Desktop/LabProject/LabProject/CloudQuery/FinalResultRichard.csv  darren@192.168.1.110:/home/darren/Desktop/LabProject/LabProject/CloudQuery/
+expect "darren@192.168.1.110's password:"
 send "0116166224\r"
-expect "darren@192.168.1.114's password:"
+expect "darren@192.168.1.110's password:"
 send "0116166224\r"
-expect "darren@192.168.1.114's password:"
+expect "darren@192.168.1.110's password:"
 send "0116166224\r"
